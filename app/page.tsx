bb32d62aaaf8db13b2d0b4b5e0c81f8a0ce49d0c
Anubhav Nepal <69887655+anubhavnepal@users.noreply.github.com>
2024-08-30 06:36:04 +0545

    Update page.tsx
    updated signincontainer's width for smaller devices

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   
   return (
     <section className="shadow-xl shadow-neutral-800 login-container h-screen flex justify-center items-center">
-      <div className="signin_container bg-white/90 w-1/3 h-fit py-5 rounded-lg">
+      <div className="signin_container bg-white/90 md:w-1/3 h-fit py-5 rounded-lg">
         <h1 className="text-center font-bold text-2xl text-gray-800 mb-2">
           User Login
         </h1>
